7156f8fbf6e398780b1a49cfc417d62392fbf068
lowkey <xinghao.wang@tosee.cn>
2022-04-26 18:45:30 +0800

    [triplets] mac add bitcode flag

--- triplets/x64-osx.cmake
+++ triplets/x64-osx.cmake
@@ -4,4 +4,7 @@ set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES x86_64)
+set(VCPKG_CXX_FLAGS "-fembed-bitcode -mmacosx-version-min=10.12")
+set(VCPKG_C_FLAGS "-fembed-bitcode -mmacosx-version-min=10.12")
+set(VCPKG_LINKER_FLAGS "-fembed-bitcode -mmacosx-version-min=10.12")
 
