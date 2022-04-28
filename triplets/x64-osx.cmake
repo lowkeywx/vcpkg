b2560e3ac31e631710569f31cc44cd827ca395e8
lowkey <xinghao.wang@tosee.cn>
2022-04-28 14:24:31 +0800

    [vcpkg] [triplete] osx set minosversion=10.12

--- triplets/x64-osx.cmake
+++ triplets/x64-osx.cmake
@@ -2,9 +2,10 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
+set(VCPKG_OSX_DEPLOYMENT_TARGET 10.12)
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES x86_64)
-set(VCPKG_CXX_FLAGS "-fembed-bitcode -mmacosx-version-min=10.12")
-set(VCPKG_C_FLAGS "-fembed-bitcode -mmacosx-version-min=10.12")
-set(VCPKG_LINKER_FLAGS "-fembed-bitcode -mmacosx-version-min=10.12")
+set(VCPKG_CXX_FLAGS "-fembed-bitcode")
+set(VCPKG_C_FLAGS "-fembed-bitcode")
+set(VCPKG_LINKER_FLAGS "-fembed-bitcode")
 
